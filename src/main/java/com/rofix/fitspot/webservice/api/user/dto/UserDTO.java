7c36ef9c6f391570f831975b932d1cabb16918c2
aram <6aram@naver.com>
2025-08-13 21:58:59 +0900

    [refactor] UserDTO 위치 수정

--- src/main/java/com/rofix/fitspot/webservice/api/user/dto/UserDTO.java
+++ src/main/java/com/rofix/fitspot/webservice/api/user/dto/UserDTO.java
@@ -1,4 +1,4 @@
-package com.rofix.fitspot.webservice.api.user.entity;
+package com.rofix.fitspot.webservice.api.user.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
